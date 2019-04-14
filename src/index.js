bce9916d4cd97fb985637eac8c0276563f01835a
Alexander Kuchuk <dafaqtx@gmail.com>
2019-04-14 20:54:57 +0300

    start work with swapi

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-console.log('clear')
+class SwapiService {
+  _apiBase = "https://swapi.co/api";
+
+  async gerResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+
+    if (!res.ok) {
+      throw new Error(`Could no fetch ${url}`);
+    }
+
+    return await res.json();
+  }
+
+  async getAllPeople() {
+    const res = await this.gerResource(`/people/`);
+    return res.results;
+  }
+
+  getPerson(id) {
+    return this.gerResource(`/people/${id}`);
+  }
+
+  async getAllPlanets() {
+    const res = await this.gerResource(`/planets/`);
+    return res.results;
+  }
+
+  getPlanet(id) {
+    return this.gerResource(`/planets/${id}`);
+  }
+
+  async getAllStarships() {
+    const res = await this.gerResource(`/starships/`);
+    return res.results;
+  }
+
+  getStarship(id) {
+    return this.gerResource(`/starships/${id}`);
+  }
+}
+
+const swapi = new SwapiService()
+
+swapi.getPerson(15).then((person) => console.log(person.name))
